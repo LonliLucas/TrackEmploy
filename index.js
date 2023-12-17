20e839dfa86afd2b11bd60daf4734a181cd319df
LonliLucas <lukej.gilless@gmail.com>
2023-12-16 20:11:47 -0700

    added code to addDepartment function

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ const db = mysql.createConnection({
 function viewDepartments() {
   db.query(
     {
-      sql: `SELECT department_name FROM departments`,
+      sql: `SELECT department_name, id FROM departments`,
       rowsAsArray: true,
     },
     (err, results) => {
       if (err) {
         console.error(err);
       } else {
-        results.unshift(["Departments"]);
+        results.unshift(["Departments", "Dept ID"]);
         console.log(table(results));
         menu();
       }
@@ -32,14 +32,15 @@ function viewDepartments() {
 function viewRoles() {
   db.query(
     {
-      sql: `SELECT title, salary FROM roles`,
+      sql: `SELECT r.title, r.id, r.salary, dep.department_name FROM roles r
+      LEFT JOIN departments dep ON dep.id = r.department_id`,
       rowsAsArray: true,
     },
     (err, results) => {
       if (err) {
         console.error(err);
       } else {
-        results.unshift(["Role", "Salary"]);
+        results.unshift(["Role", "Role ID", "Salary", "Department"]);
         console.log(table(results));
         menu();
       }
@@ -50,14 +51,25 @@ function viewRoles() {
 function viewEmployees() {
   db.query(
     {
-      sql: `SELECT first_name, last_name FROM employees`,
+      sql: `SELECT emp.id, CONCAT(emp.first_name, ' ', emp.last_name) AS employee_name, r.title, dep.department_name, r.salary, CONCAT (man.first_name, ' ', man.last_name)
+      FROM employees emp
+      LEFT JOIN roles r ON r.id = emp.role_id
+      LEFT JOIN departments dep ON dep.id = r.department_id
+      LEFT JOIN employees man ON man.id = emp.manager_id`,
       rowsAsArray: true,
     },
     (err, results) => {
       if (err) {
         console.error(err);
       } else {
-        results.unshift(["First Name", "Last Name"]);
+        results.unshift([
+          "ID",
+          "Full Name",
+          "Title",
+          "Department",
+          "Salary",
+          "Reports To",
+        ]);
         console.log(table(results));
         menu();
       }
@@ -66,7 +78,28 @@ function viewEmployees() {
 }
 
 function addDepartment() {
-  db.query({ sql: `` });
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "newDepartment",
+        message: "What is the name of the new Department?:",
+      }
+    ])
+    .then((answers) => {
+      db.query(
+        "INSERT INTO departments SET ?",
+        { department_name: answers.newDepartment },
+        (err) => {
+          if (err) {
+            console.error(err);
+          }
+          
+          console.log("Added new department.")
+          menu();
+        }
+      );
+    });
 }
 
 function addRole() {
@@ -77,27 +110,29 @@ function addEmployee() {
   db.query({ sql: `` });
 }
 
+function updateEmployee() {
+  db.query({ sql: `` });
+}
+
 async function menuTitle() {
   const title = "TrackEmploy";
 
   return new Promise((resolve, reject) => {
-  ascii.font(title, "doom", (err, rendered) => {
-    if (err) {
-      console.error(err);
-      reject();
-    }
-    else {
-      console.log(rendered);
-      resolve();
-    }
-  })
-})
+    ascii.font(title, "doom", (err, rendered) => {
+      if (err) {
+        console.error(err);
+        reject();
+      } else {
+        console.log(rendered);
+        resolve();
+      }
+    });
+  });
 }
 
 async function menu() {
-  await menuTitle();
-  try {
-    const options = await inquirer.prompt([{
+  const options = await inquirer.prompt([
+    {
       type: "list",
       name: "menu",
       message: "Pick an option from this list!",
@@ -108,24 +143,48 @@ async function menu() {
         "Add a new Department",
         "Add a new Role",
         "Add a new Employee",
-        "X"
+        "Update an Employee",
+        "X",
       ],
     },
   ]);
 
   switch (options.menu) {
-    case "View Departments": viewDepartments(); break;
-    case "View Roles": viewRoles(); break;
-    case "View Employees": viewEmployees(); break;
-    case "Add a new Department": addDepartment(); break;
-    case "Add a new Role": addRole(); break;
-    case "Add a new Employee": addEmployee(); break;
-    case "X": db.end(); console.log("Thank you"); break;
+    case "View Departments":
+      viewDepartments();
+      break;
+    case "View Roles":
+      viewRoles();
+      break;
+    case "View Employees":
+      viewEmployees();
+      break;
+    case "Add a new Department":
+      addDepartment();
+      break;
+    case "Add a new Role":
+      addRole();
+      break;
+    case "Add a new Employee":
+      addEmployee();
+      break;
+    case "Update an Employee":
+      updateEmployee();
+      break;
+    case "X":
+      db.end();
+      console.log("Closing Trackemploy...");
+      break;
   }
+}
 
+async function application() {
+  await menuTitle();
+  try {
+    menu();
   } catch (err) {
     console.error(err);
   }
 }
 
-menu();
+application();
